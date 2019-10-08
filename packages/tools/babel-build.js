6f6709fb654bfd95fed7f76f72af3fee10768d39
thebigbrain <uibrick@126.com>
2019-10-08 21:40:28 +0800

    .

--- packages/tools/babel-build.js
+++ packages/tools/babel-build.js
@@ -3,8 +3,20 @@ const Path = require('path')
 const babel = require("@babel/core")
 const traverse = require('@babel/traverse').default
 const generate = require('@babel/generator').default
+const mkdirp = require('mkdirp')
+const commander = require('commander')
 
-const filename = process.argv[2]
+const program = new commander.Command()
+program
+  .version('1.0.0', '-v, --version')
+  .usage('[OPTIONS]')
+  .option('-e, --entry <entry>', 'entry file')
+  .option('-n, --name <name>', 'module name')
+  .parse(process.argv)
+
+const filename = program.entry
+const moduleName = program.name
+const prefix = `local@${moduleName}`
 const out = Path.resolve('./dist', filename)
 const root = Path.resolve('.')
 let currentPathStack = [Path.resolve(filename)]
@@ -42,7 +54,7 @@ function transformJs(code) {
 function processJs() {
   let resource = currentPathStack.pop()
   resource = require.resolve(resource)
-  let id = `local:${Path.relative(root, resource)}`
+  let id = `${prefix}:${Path.relative(root, resource)}`
   let code = readJsFile(resource)
   if (localModules[id] == null) {
     let result = transformJs(code)
@@ -57,8 +69,12 @@ function readJsFile(filename) {
 
 function writeOut(result) {
   let code = `// load local modules early
-  window.__localModules=window.__localModules || {};
-Object.assign(window.__localModules, ${JSON.stringify(localModules)})
+window.__localModules = window.__localModules || {};
+window.__localModules['${prefix}'] = window.__localModules['${prefix}'] || {}
+Object.assign(
+  window.__localModules['${prefix}'],
+  ${JSON.stringify(localModules)}
+)
 
 ${result.code}
 `

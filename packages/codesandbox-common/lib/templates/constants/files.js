419ca51ae58044901e57df61f957d97f74ebb103
thebigbrain <uibrick@126.com>
2019-10-25 21:09:53 +0800

    .

--- packages/codesandbox-common/lib/templates/constants/files.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.HIDDEN_DIRECTORIES = void 0;
-var HIDDEN_DIRECTORIES = ['.codesandbox'];
-exports.HIDDEN_DIRECTORIES = HIDDEN_DIRECTORIES;
--- packages/codesandbox-common/lib/utils/ci.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.CSB_PKG_PROTOCOL = void 0;
-var CSB_PKG_PROTOCOL = /https:\/\/pkg(-staging)?\.csb.dev/;
-exports.CSB_PKG_PROTOCOL = CSB_PKG_PROTOCOL;
